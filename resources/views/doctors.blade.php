cc5214cf3d5eb7d9ed4f5ada19cd224888335dd9
ShrutiSemwal <semwalshruti450@gmail.com>
2021-06-29 16:46:29 +0530

    Added error page

--- resources/views/doctors.blade.php
+++ resources/views/doctors.blade.php
@@ -1,3 +1,6 @@
+@extends('layout')
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,73 +21,30 @@
         <table id="example" class="table table-striped table-bordered nowrap" style="width:100%">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Office</th>
-                    <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Department</th>
+                    <th>Contact No.</th>
                 </tr>
             </thead>
             <tbody>
+            @foreach($doc as $d)
                 <tr>
-                    <td>Tiger Nixon</td>
-                    <td>System Architect</td>
-                    <td>Edinburgh</td>
-                    <td>61</td>
-                    <td>2011/04/25</td>
-                    <td>$320,800</td>
+                    <td>{{$d->fname}}</td>
+                    <td> {{$d->lname}}</td>
+                    <td>{{$d->dept}}</td>
+                    <td> {{$d->phone}}</td>
+                   
                 </tr>
-                <tr>
-                    <td>Garrett Winters</td>
-                    <td>Accountant</td>
-                    <td>Tokyo</td>
-                    <td>63</td>
-                    <td>2011/07/25</td>
-                    <td>$170,750</td>
-                </tr>
-                <tr>
-                    <td>Ashton Cox</td>
-                    <td>Junior Technical Author</td>
-                    <td>San Francisco</td>
-                    <td>66</td>
-                    <td>2009/01/12</td>
-                    <td>$86,000</td>
-                </tr>
-                <tr>
-                    <td>Cedric Kelly</td>
-                    <td>Senior Javascript Developer</td>
-                    <td>Edinburgh</td>
-                    <td>22</td>
-                    <td>2012/03/29</td>
-                    <td>$433,060</td>
-                </tr>
-                <tr>
-                    <td>Airi Satou</td>
-                    <td>Accountant</td>
-                    <td>Tokyo</td>
-                    <td>33</td>
-                    <td>2008/11/28</td>
-                    <td>$162,700</td>
-                </tr>
-                <tr>
-                    <td>Brielle Williamson</td>
-                    <td>Integration Specialist</td>
-                    <td>New York</td>
-                    <td>61</td>
-                    <td>2012/12/02</td>
-                    <td>$372,000</td>
-                </tr>
-
+                </br>
+                @endforeach
                 </tbody>
             <tfoot>
                 <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Office</th>
-                    <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
+                    <th> First Name</th>
+                    <th>Last Name</th>
+                    <th>Department</th>
+                    <th>Contact No.</th>
                 </tr>
             </tfoot>
         </table>
@@ -99,19 +59,14 @@
 
     <script>
         $(document).ready(function() {
-            var table = $('#example').DataTable( {
+            var table = $('#example').DataTable({
                 responsive: true
-            } );
+            });
         
             new $.fn.dataTable.FixedHeader( table );
-        } );
+         });
     </script>
 
-    @foreach($doc as $d)
-    {{$d->fname}}
-    {{$d->lname}}
-    {{$d->dept}}
-    {{$d->phone}} </br>
-    @endforeach
 </body>
 </html>
+@endsection
